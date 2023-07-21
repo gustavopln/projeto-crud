2011309cd00a9b66c7dd9c0fdf2a8efee205fd9d
gustavopln <gustavopln@gmail.com>
2023-07-20 23:54:26 -0300

    Criando estrutura da Model e Controller

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,9 @@ function connect(){
     //mongoose.set('useNewUrlParser', true)
     //mongoose.set('useUnifiedTopology', true)
     
-    mongoose.connect('mongodb://localhost:27017')
+    mongoose.connect('mongodb://localhost:27017/primeiro-projeto')
     
+    // Não é um método, é assim mesmo
     const db = mongoose.connection
     
     // Caso a conexão seja realizada com sucesso
@@ -13,7 +14,7 @@ function connect(){
         console.log('Conected to database!')
     })
     
-    // Caso der erro na conexão
+    // Caso der erro na conexão, o bind serve para conectar a msg de erro com o Connection error:
     db.on('error', console.error.bind(console,'Connection error: '))
 }
 
