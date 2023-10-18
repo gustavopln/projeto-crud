2e5dc26c149fb67f31ee4665c7e74af30c04fe2f
Gustavo Paulino <cast.gustavo.utic@sebrae.com.br>
2023-10-18 15:56:49 -0300

    adicionando feature de cadastro, edição listagem e remoção de clientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,22 @@
 const router = require('express').Router()
 const CustomersController = require('../controllers/customers')
+const IndexController = require('../controllers/index')
 
 // rotas
-router.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Titulo Teste'
-    })
-})
+router.get('/', IndexController.index)
 
-router.get('/register', (req, res) => {
-    res.render('register', {
-        title: 'Cadastro de Clientes'
-    })
-})
-
+// registro
+router.get('/register', CustomersController.index)
 router.post('/register/add', CustomersController.add)
+
+// listar
+router.get('/list', CustomersController.list)
+
+// editar
+router.get('/edit', CustomersController.formEdit)
+router.post('/edit/:id', CustomersController.edit)
+
+// excluir
+router.get('/remove/:id', CustomersController.remove)
 
 module.exports = router
